28774c6fc179742eec30ad5d9dbaa5e6c80392bb
Dominique Eav <dom@lee.(none)>
2009-08-14 18:39:20 +0700

    Changed destination folder for local site update

--- update-website.sh
+++ update-website.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-date && mkdir -p /home/dom/perso/ladypenh/django-appengine/cache && cd /home/dom/perso/ladypenh/django-appengine/cache && /usr/bin/wget --mirror -N -nH http://ladypenh.appspot.com/index.html http://ladypenh.appspot.com/ijustwanttheprogramthanks.html http://ladypenh.appspot.com/feed/tomorrow_overview.html && rsync -rvu . ladypenh@ladypenh.com:www/
+date && mkdir -p /home/dom/perso/ladypenh/cache && cd /home/dom/perso/ladypenh/cache && /usr/bin/wget --mirror -N -nH http://ladypenh.appspot.com/index.html http://ladypenh.appspot.com/ijustwanttheprogramthanks.html http://ladypenh.appspot.com/feed/tomorrow_overview.html && rsync -rvu . ladypenh@ladypenh.com:www/
